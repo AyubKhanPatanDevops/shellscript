70caf117b80a6db4a3b33fc91266201cfbd9d608
AyubDevopss <ayubkhan.pathanns@gmail.com>
2025-07-05 12:20:55 +0530

    ayubmesasge

---
+++ functions.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+loops and conditions and 
+
+shell is all the files can be devloped in file is called shells that is shell scri[t 
+
+we hve been automateing using the memeiry and disc issue using the shell script ]
---
+++ varibles.sh
@@ -0,0 +1,6 @@
+#bin/bash 
+hi please check varibles in shell script 
+
+its kind of defination to all the data persons 
+
+plese check have a look varibles 
