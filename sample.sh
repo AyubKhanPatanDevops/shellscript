62ffe94d1f27f2085c5cb0f561ba3c4aa136957e
AyubDevopss <ayubkhan.pathanns@gmail.com>
2025-07-05 12:33:31 +0530

    ayubmesasge

---
+++ sample.sh
@@ -0,0 +1,29 @@
+#bin/bash!
+
+Hello every one please checking the data 
+
+among this data please having the look 
+
+first need to creat to repo and do create a folder
+then you  clone on the laptip then open in vs code 
+
+then add using git add command 
+
+then using git commit nice mesasge git commit -m 
+
+git push -u origin main 
+
+
+commadns are below 
+
+git clone 
+this command can clone the git repo
+
+
+git add command adding th data to repo 
+
+then commit the git commad # git commit -m "messagae"
+
+git push -u origin main this command can push the git command 
+
+this is the commands mainly this is the process 
