cb8601553f8226bad25c456cc71aea2080989748
AyubDevopss <ayubkhan.pathanns@gmail.com>
2025-07-07 13:21:59 +0530

    learningdevops

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: hello suresh how are you?"
+echo "suresh:: hellore ramesh iam fine how are you?"
+echo "Ramesh:: iam fine to howis yourwork"
+echo "suresh:: not bad iam thinkig toupgrade devops'
