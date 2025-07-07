f28a48bad98a657ebeca3a0122625bb839bd90b2
AyubDevopss <ayubkhan.pathanns@gmail.com>
2025-07-07 16:28:46 +0530

    learningdevops

---
+++ 06-varibles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter user name ::"
+
+read USERNAME #here USERNAME is varible 
+
+echo "please enterpassword::"
+
+read PASSWORD
+
+echo "username is:$USERNAME, password is: $PASSWORD"
