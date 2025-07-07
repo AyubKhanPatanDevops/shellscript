5da0a952f5536366e54ce5a5d3e69cdd29309023
AyubDevopss <ayubkhan.pathanns@gmail.com>
2025-07-07 13:41:16 +0530

    learningdevops

--- 03-varibles.sh
+++ 03-varibles.sh
@@ -6,4 +6,4 @@ PERSON2=Suresh
 echo "$PERSON1:: hello $PERSON2 how are you?"
 echo "$PERSON2:: hellore $PERSON1 iam fine how are you?"
 echo "$PERSON1:: iam fine to howis yourwork"
-echo "$PERSON2:: not bad iam thinkig toupgrade devops'
+echo "$PERSON2:: not bad iam thinkig toupgrade devops"
