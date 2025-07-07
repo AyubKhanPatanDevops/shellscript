66fb0fa08c45c55dc72d37477018189f46967e11
AyubDevopss <ayubkhan.pathanns@gmail.com>
2025-07-07 13:38:51 +0530

    learningdevops

---
+++ 03-varibles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh #no space between = and value
+PERSON2=Suresh 
+
+echo "$PERSON1:: hello $PERSON2 how are you?"
+echo "$PERSON2:: hellore $PERSON1 iam fine how are you?"
+echo "$PERSON1:: iam fine to howis yourwork"
+echo "$PERSON2:: not bad iam thinkig toupgrade devops'
