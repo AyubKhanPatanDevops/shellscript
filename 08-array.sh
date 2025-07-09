2de30d83c38faf000433de948c80a384448a1484
AyubDevopss <ayubkhan.pathanns@gmail.com>
2025-07-09 16:18:24 +0530

    learningdevops

---
+++ 08-array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+HOUSENAMES=("AYUB" "AKBAR" "AMEERKHAN")
+
+#size of above array is 3.
+#index are 0,1,2
+# list always start with 0.
+
+echo "first name  is: ${NAMES[0]}" 
+echo "first name  is: ${NAMES[1]}" 
+echo "first name  is: ${NAMES[2]}"
+echo "first name  is: ${NAMES[@]}" 
