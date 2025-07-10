d9d83b19b2139e7e5c6f9f8bfc4d6db2e331cfaf
AyubDevopss <ayubkhan.pathanns@gmail.com>
2025-07-10 13:21:47 +0530

    learningdevops

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,9 +2,9 @@
 
 NUMBER=$0
 
-if  [ $NUMBER -gt 20 ]
+if [ $NUMBER -gt 10 ]
 then
-   echo "given number $NUMBER is Greater than 20"
+   echo "given number $NUMBER is Greater than 10"
 else
-    echo  "given number $NUMBER is less than 20"
+    echo  "given number $NUMBER is less than 10"
   fi
