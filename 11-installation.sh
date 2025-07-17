fa0bb09e09625bd210b4f21d03992a25b96268ce
AyubDevopss <ayubkhan.pathanns@gmail.com>
2025-07-17 10:36:31 +0530

    learningdevops

---
+++ 11-installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run this script with root acess."
+    exit 1 # manually exit if error comes 
+else 
+    echo "you are super user."  
+fi       
+
+dnf install mysql -y
+
+
+
+
+
+echo "is script proceeding?"
